aaf1e17028d923516397f259e6e137b35a637593
Eugene Zorin <evgzor@mail.ru>
2017-03-11 12:35:24 +0300

    1. support upside down 2. removed comments

--- PixaBayImageViewer/PixaBayImageViewer/ViewControllers/DetailImageViewController.m
+++ PixaBayImageViewer/PixaBayImageViewer/ViewControllers/DetailImageViewController.m
@@ -108,15 +108,4 @@ static NSString* const kParameterNumberOfComments = @"Comments";
   return [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 2)];
 }
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
